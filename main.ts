22ad132bc1b623b906443542fa7e4b0b25d2b67a
pete-wildberger <pete.wildberger@gmail.com>
2018-04-26 15:04:13 -0500

    rm unneeded imports

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
-console.log('TS is sourced');
-import { Individual_class } from './classes/Individual.class';
-import { Parent, Parent_class } from './classes/Parent.class';
-import { Child, Child_class } from './classes/Child.class';
-import { App } from './classes/App.class';
-import * as readline from 'readline';
+console.log("TS is sourced");
+import { App } from "./classes/App.class";
+import * as readline from "readline";
 
 const main = (function(): void {
-  let app = new App('Hyaaaaah', 100);
+  let app = new App("Hyaaaaah", 100);
   app.main();
 })();
